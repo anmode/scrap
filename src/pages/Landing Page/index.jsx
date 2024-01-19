a44105a34aeac738e2d8fa57f0990c6681d31b72
khush2706 <kc220313@gmail.com>
2024-01-19 18:06:18 +0530

    - added prettier and eslint config - initial repo structure setup

---
+++ src/pages/Landing Page/index.jsx
@@ -0,0 +1,11 @@
+import styles from './index.module.css';
+
+const LandingPage = () => {
+  return (
+    <>
+      <div className={styles.bg}></div>
+    </>
+  );
+};
+
+export default LandingPage;
