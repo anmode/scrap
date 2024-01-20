555b5e1c8ba48836c3351e2ccf3bde0945f0bde2
Mahak Porwal <mahak.porwal@hackerrank.com>
2024-01-20 21:05:22 +0530

    added landing page

--- src/pages/Landing Page/index.jsx
+++ src/pages/Landing Page/index.jsx
@@ -1,9 +1,15 @@
 import styles from './index.module.css';
+import svg from '../../assets/background.svg'
+import svg2 from '../../assets/welcome-to_hackerspace.svg'
 
 const LandingPage = () => {
   return (
     <>
-      <div className={styles.bg}></div>
+      <div>
+        <div style={{ "background-image": `url(${svg})` }} className={styles.landingPage }>
+          <img src={svg2} className={styles.landingPageWelcomeImage} />
+        </div>
+      </div>
     </>
   );
 };
