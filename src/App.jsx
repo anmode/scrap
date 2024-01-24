f1fed45e7e154939812e4158e5324b7a497ac4a7
Iltwats <atul.sharma@hackerrank.com>
2024-01-24 15:47:27 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import LandingPage from "./pages/LandingPage"
 function App() {
   return (
-    <Router>
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="*" element={<ErrorPage />} />
-      <Route path="/:username" element={<InternProfilePage />} />
-    </Routes>
-  </Router>
+   <LandingPage/>
   );
 }
 
