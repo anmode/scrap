5c937de25676e7542fe6560a644f66d61302e268
Anmol <anmol2002.aa1@gmail.com>
2024-01-26 15:50:31 +0530

    fix build and added routing issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import LandingPage from "./pages/LandingPage"
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage/index';
+import ErrorPage from './pages/errorPage';
+import InternProfilePage from './pages/internProfile/internProfile';
 function App() {
   return (
-   <LandingPage/>
+    <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="*" element={<ErrorPage />} />
+      <Route path="/:username" element={<InternProfilePage />} />
+    </Routes>
+  </Router>
   );
 }
 
