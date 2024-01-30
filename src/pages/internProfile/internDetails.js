e6636f6c38dd61db205f315d8f8bfa990e9fee8c
Mahak Porwal <mahak.porwal@hackerrank.com>
2024-01-30 21:17:32 +0530

    interns page ui completed

--- src/pages/internProfile/internDetails.js
+++ src/pages/internProfile/internDetails.js
@@ -8,60 +8,58 @@ export const anmol = {
   github: 'https://github.com/anmode',
   website: '',
   instagram: '',
-  hobby: 'Bathroom singer'
+  hobby: 'Basketball'
 };
 
 export const mahak = {
   username: 'mahak',
   name: 'Mahak Porwal',
   position: 'SDE Intern',
-  city: 'Dummy City',
-  about: 'Dummy about Mahak.',
-  linkedin: 'https://linkedin.com/in/mahakporwal',
-  github: 'https://github.com/mahak',
-  website: '',
-  instagram: '',
-  hobby: 'Bathroom singer'
+  city: 'Indore',
+  about: 'Software engineer keen to design, build, solve, and democratize technology for the next billion internet users one commit at a time.',
+  linkedin: 'https://www.linkedin.com/in/mahak-porwal/',
+  github: 'https://github.com/mahakporwal02',
+  website: 'https://mahakporwal02.github.io/',
+  instagram: 'https://www.instagram.com/_mahakporwal_/',
+  hobby: 'Swimming, Travelling'
 };
 
 export const atul = {
   username: 'atul',
   name: 'Atul Sharma',
   position: 'SDE Intern',
-  city: 'Dummy City',
-  about: 'Dummy about Atul.',
-  linkedin: 'https://linkedin.com/in/atulsharma',
-  github: 'https://github.com/atul',
-  website: '',
-  instagram: '',
-  hobby: 'Bathroom singer'
+  city: 'Patna',
+  about: 'Building products that people will use',
+  linkedin: 'https://www.linkedin.com/in/1220atul/',
+  github: 'https://github.com/Iltwats',
+  website: 'https://www.atul-sharma.tech/',
+  instagram: 'https://www.instagram.com/atulsharma_01/',
+  hobby: 'Playing Basketball, Listening to music'
 };
 
 export const sumit = {
   username: 'sumit',
   name: 'Sumit Maithani',
   position: 'SDE Intern',
-  city: 'Dummy City',
-  about:
-    'Bullying Interns Bullying InternsBullying Interns Bullying InternsBullying Interns Bullying InternsBullying Interns Bullying InternsBullying Interns Bullying InternsBullying',
-  linkedin: 'https://linkedin.com/in/sumitmaithani',
-  github: 'https://github.com/sumit',
-  website: '',
-  instagram: '',
-  hobby: 'Bathroom singer'
+  city: 'Srinagar City',
+  about: 'Dummy about me.',
+  linkedin: 'https://www.linkedin.com/in/sumit-maithani-103119228/',
+  github: 'https://github.com/Sumitmaithani',
+  website: 'https://sumitmaithani.onrender.com',
+  instagram: 'https://www.instagram.com/sumitmaithani1',
+  hobby: 'hobby'
 };
 
 export const suyash = {
   username: 'suyash',
   name: 'Suyash Purwar',
   position: 'SDE Intern',
-  city: 'Dummy City',
-  about: 'Dummy about Suyash.',
-  linkedin: 'https://linkedin.com/in/suyashpurwar',
-  github: 'https://github.com/suyash',
-  website: '',
-  instagram: '',
-  hobby: 'Bathroom singer'
+  city: 'Etawah',
+  about: "I'm the kind of backend engineer she tells you not to worry about. I build systems that scale to the moon and make your database question their limits.",
+  linkedin: 'https://www.linkedin.com/in/suyash-purwar/',
+  github: 'https://github.com/suyash-purwar',
+  twitter: 'https://twitter.com/suyashpurwar06',
+  hobby: 'low level engineering'
 };
 
 export const khushboo = {
@@ -79,54 +77,54 @@ export const khushboo = {
 
 export const shreya = {
   username: 'shreya',
-  name: 'Shreya',
+  name: 'Shreya Srivastava',
   position: 'SDE Intern',
-  city: 'Dummy City',
-  about: 'Dummy about Shreya.',
-  linkedin: 'https://linkedin.com/in/shreya',
-  github: 'https://github.com/shreya',
-  website: '',
+  city: 'Ahmedabad',
+  about: 'All I know is how to code',
+  linkedin: 'https://www.linkedin.com/in/this-is-shreya/',
+  github: 'https://github.com/this-is-shreya',
+  website: 'https://codecafe-8v33.onrender.com/',
   instagram: '',
-  hobby: 'Bathroom singer'
+  hobby: 'Reading books, listening to music, watching movies'
 };
 
 export const sindhu = {
   username: 'sindhu',
-  name: 'Sindhu',
+  name: 'Sindhu Inti',
   position: 'SDE Intern',
-  city: 'Dummy City',
-  about: 'Dummy about Sindhu.',
-  linkedin: 'https://linkedin.com/in/sindhu',
-  github: 'https://github.com/sindhu',
+  city: 'Tanuku',
+  about: 'Hello, I\'m Sindhu  full stack web developer.I say myself as a stargrazer, loves to read both sci- fi and non-fiction books and a huge fan of MCU ',
+  linkedin: 'https://linkedin.com/in/sindhuinti',
+  github: 'https://github.com/sindhuinti',
   website: '',
   instagram: '',
-  hobby: 'Bathroom singer'
+  hobby: 'Reading Sci-fi books'
 };
 
 export const naman = {
   username: 'naman',
   name: 'Naman Lakhwani',
   position: 'SDE Intern',
-  city: 'Dummy City',
-  about: 'Dummy about Naman.',
-  linkedin: 'https://linkedin.com/in/namanlakhwani',
-  github: 'https://github.com/naman',
-  website: '',
-  instagram: '',
-  hobby: 'Bathroom singer'
+  city: 'Datia',
+  about: 'I like to dive deep into systems-level engineering',
+  linkedin: 'https://www.linkedin.com/in/naman2001/',
+  github: 'https://github.com/Namanl2001',
+  website: 'https://namanlakhwani.dev/',
+  instagram: 'https://www.instagram.com/lakhwani_jii/',
+  hobby: 'Reading books'
 };
 
 export const akash = {
   username: 'akash',
   name: 'Akash',
   position: 'Product Design Intern',
-  city: 'Dummy City',
-  about: 'Dummy about Akash.',
-  linkedin: 'https://linkedin.com/in/akash',
-  github: 'https://github.com/akash',
-  website: '',
-  instagram: '',
-  hobby: 'Bathroom singer'
+  city: 'Alappuzha, Kerala',
+  about: 'Hello guys, Myself Akash. I\'m from Alappuzha, Kerala. I\'m a design graduate. I am a highly motivated and versatile Product Designer. My expertise lies in creating exceptional user interfaces (UI), delivering engaging user experiences (UX), and crafting captivating motion designs. I am passionate about pushing creative boundaries and leveraging my diverse skill set to contribute to hackerrank\'s team\'s success. ',
+  linkedin: 'https://www.linkedin.com/in/akashchennessery/',
+  github: 'N/A',
+  website: 'N/A',
+  instagram: 'https://www.instagram.com/dooddler/',
+  hobby: 'Sakteboarding',
 };
 
 export const arshad = {
