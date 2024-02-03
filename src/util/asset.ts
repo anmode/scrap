5ed1120b6a708f83953595645a862516803a869c
Anmol <anmol2002.aa1@gmail.com>
2024-02-03 20:07:02 +0530

    path error fix

--- src/util/asset.ts
+++ src/util/asset.ts
@@ -1,6 +1,6 @@
 const getAssetPath = (relativePath: string): string => {
   if (process.env.NODE_ENV === "development") {
-    return `https://interns2024.pages.dev/assets${relativePath}`;
+    return `https://interns2024.pages.dev/assets/${relativePath}`;
   } else {
     return `/asset/${relativePath}`;
   }
