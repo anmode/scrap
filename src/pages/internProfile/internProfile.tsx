0b50e0aa12b6e478b2ab3c72eabdf57c568aaafb
Anmol <anmol2002.aa1@gmail.com>
2024-02-04 11:37:38 +0530

    Bug:3 fixed --> internsPage

--- src/pages/internProfile/internProfile.tsx
+++ src/pages/internProfile/internProfile.tsx
@@ -67,7 +67,7 @@ const InternProfilePage: React.FC = () => {
       <Helmet>
         <meta property="og:title" content={`${internProfile?.name}'s Profile`} />
         <meta property="og:description" content={`Learn more about ${internProfile?.name}`} />
-        <meta property="og:image" content={getAssetPath(`/internAvatar/${username}.svg`)} />
+        <meta property="og:image" content={getAssetPath(`internProfileAvatar/${username}.svg`)} />
         <meta
           property="og:url"
           content={`https://interns2024.pages.dev/interns/2024/${username}`}
@@ -91,7 +91,7 @@ const InternProfilePage: React.FC = () => {
             <div className={styles.img_container}>
               <img
                 className={styles.internprofile__internAvatar}
-                src={getAssetPath(`/internAvatar/${username}.svg`)}
+                src={getAssetPath(`internProfileAvatar/${username}.svg`)}
                 alt={`Profile of ${internProfile?.name}`}
               />
             </div>
